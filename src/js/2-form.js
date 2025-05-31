e9fa6a6b249a8976a644a3b3d5c5406c56b90db9
Borys Savenok <horosh.narod@gmail.com>
2025-06-01 01:56:55 +0300

    home work number 9

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+const form = document.querySelector(".feedback-form");
+const formData = { email: "", message: "" };
+const key = "feedback-form-state";
+populateTextArea();
+form.addEventListener("input", formInfoSaver);
+form.addEventListener("submit", submitHandler);
+function formInfoSaver(e) {
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;
+    localStorage.setItem(key, JSON.stringify(formData));
+}
+function populateTextArea() {
+    const saved  = JSON.parse(localStorage.getItem(key));
+    if (!saved) return;
+    if (message.email) {
+    form.elements.email.value = message.email;
+    } 
+    if (message.message) {
+    form.elements.message.value = message.message; 
+    } 
+    }
+
+function submitHandler(e) {
+    e.preventDefault();
+    if (formData.email.trim() === "" || formData.message.trim() === "") {
+        return alert("Fill please all fields")
+    }
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(key);
+    formData.email = "";
+    formData.message = "";
+}
+    
